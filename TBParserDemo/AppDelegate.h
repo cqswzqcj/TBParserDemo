2b31839c2124ffdd4bed6216cafdf0ee8f787e7e
cy <cqswzqcj@163.com>
2017-11-03 16:12:04 +0800

    Initial Commit

---
+++ TBParserDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TBParserDemo
+//
+//  Created by cy on 2017/11/3.
+//  Copyright © 2017年 cy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
